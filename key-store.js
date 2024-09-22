f9ee86adb3e5268a811ac471b31420801e093958
oriastanjung <oriastan999@gmail.com>
2024-09-22 11:32:44 +0700

    feat: authentication

--- key-store.js
+++ key-store.js
@@ -1,11 +1,18 @@
-const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
-const fs = require('fs');
-const shortid = require('shortid');
+const VALID_KEYS_PATH = __dirname + "/valid-keys.txt";
+const fs = require("fs");
+const shortid = require("shortid");
 // To generate a unique API KEY, use shortid.generate()
-const LINE_ENDING = require('os').EOL;
-
+const LINE_ENDING = require("os").EOL;
 
 module.exports = function (req, res) {
+  const apiKey = shortid.generate();
+  
+  fs.appendFile(VALID_KEYS_PATH, apiKey + LINE_ENDING, function (err) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Internal Server Error");
+    }
 
+    return res.status(201).json({ apiKey });
+  });
 };
-
--- middleware.js
+++ middleware.js
@@ -1,5 +1,23 @@
-const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
-
+const path = require('path');
+const VALID_KEYS_PATH = path.join(__dirname, '/valid-keys.txt');
+const fs = require('fs');
 module.exports = function (req, res, next) {
+    const key = req.headers['x-api-key'];
 
+    if(!key){
+        return res.status(401).json({ message: 'API key is required' });
+    }
+    else{
+        fs.readFile(VALID_KEYS_PATH,'utf8', (err, data) => {
+            if(err){
+                return res.status(500).json({ message: 'Internal Server Error' });
+            }
+
+            const validKeys = data.split('\n').map(c => c.trim());
+            if(!validKeys.includes(key)){
+                return res.status(401).json({ message: 'Invalid API key' });
+            }
+            next();
+        })
+    }
 };
